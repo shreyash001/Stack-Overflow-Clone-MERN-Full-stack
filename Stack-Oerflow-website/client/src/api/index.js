c2cefd626949bf4fff383dd949d851e583bac81b
Shreyash Singh <65494781+shreyash001@users.noreply.github.com>
2023-01-28 22:36:07 +0530

    Token issue solved

--- Stack-Oerflow-website/client/src/api/index.js
+++ Stack-Oerflow-website/client/src/api/index.js
@@ -6,6 +6,7 @@ API.interceptors.request.use( (req) => {
     if(localStorage.getItem('Profile')){
         req.headers.authorization = `Bearer ${JSON.parse(localStorage.getItem('Profile')).token}`
     }
+    return req;
 })
 
 export const logIn = (authData) => API.post('/user/login',authData);
